adad797ad6b537e750b080b3ee9aa139fd553280
Michael Kohler <me@michaelkohler.info>
2017-12-05 21:41:31 +0100

    Add s3 cleanup alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,2 +1,2 @@
 alias s3sync="aws s3 sync . s3://mk-cloud/ --profile default --exclude '._*' --exclude '*/._*'"
-
+alias s3clean="aws s3 rm s3://mk-cloud/ --recursive --exclude '*' --include '._*' --include '*/._*'"
