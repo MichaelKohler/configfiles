f3d8769977f3ec8289cdb512c5bb835789428c4f
Michael Kohler <me@michaelkohler.info>
2017-12-18 19:20:42 +0100

    Add aliases for s3 sync

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,2 +1,2 @@
-alias s3sync="aws s3 sync . s3://mk-cloud/ --profile default --exclude '._*' --exclude '*/._*'"
-alias s3clean="aws s3 rm s3://mk-cloud/ --recursive --exclude '*' --include '._*' --include '*/._*'"
+alias s3sync="aws s3 sync . s3://mk-cloud/ --profile default --exclude '._*' --exclude '*/._*' --exclude '.DS_Store' --exclude '*/.DS_Store'"
+alias s3clean="aws s3 rm s3://mk-cloud/ --recursive --exclude '*' --include '._*' --include '*/._*' --include '.DS_Store' --include '*/.DS_Store'"
