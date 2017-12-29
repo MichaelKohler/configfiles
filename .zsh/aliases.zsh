a4f53c44b4c25c34682a7fd2bed4358c473b4bf3
Michael Kohler <me@michaelkohler.info>
2017-12-30 00:18:37 +0100

    Add flushnetwork alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,2 +1,4 @@
 alias s3sync="aws s3 sync . s3://mk-cloud/ --profile default --exclude '._*' --exclude '*/._*' --exclude '.DS_Store' --exclude '*/.DS_Store'"
 alias s3clean="aws s3 rm s3://mk-cloud/ --recursive --exclude '*' --include '._*' --include '*/._*' --include '.DS_Store' --include '*/.DS_Store'"
+alias flushnetwork="sudo ifconfig en0 down && sudo route flush && sudo ifconfig en0 up"
+
