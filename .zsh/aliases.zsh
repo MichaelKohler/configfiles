6f11e6f001a95f01c12296d0a1b6ef9fa76cb3c7
Michael Kohler <me@michaelkohler.info>
2018-12-31 17:05:49 +0200

    Update

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,3 +2,5 @@ alias s3sync="aws s3 sync . s3://mk-cloud/ --profile default --exclude '._*' --e
 alias s3clean="aws s3 rm s3://mk-cloud/ --recursive --exclude '*' --include '._*' --include '*/._*' --include '.DS_Store' --include '*/.DS_Store'"
 alias flushnetwork="sudo ifconfig en0 down && sudo route flush && sudo ifconfig en0 up"
 
+alias enc="tar -cvf enc.tar.gz enc && gpg -c enc.tar.gz && rm -rf enc enc.tar.gz"
+alias dec="gpg -d enc.tar.gz.gpg > enc.tar.gz && tar -xvf enc.tar.gz"
