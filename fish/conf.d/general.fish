b8d8e57c717871cc11011fd604910071f67d5e68
Michael Kohler <me@michaelkohler.info>
2019-04-24 00:47:12 +0200

    Add fish stuff

---
+++ fish/conf.d/general.fish
@@ -0,0 +1,6 @@
+
+set -g theme_nerd_fonts yes
+set theme_color_scheme terminal-dark
+set -g theme_display_hg yes
+
+nvm use 11.10.0
---
+++ fish/setup-fish.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+brew install fish
+echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+
+rm -rf ~/.config/fish/conf.d
+ln -s ~/configfiles/fish/conf.d ~/.config/fish/conf.d
+ln -s ~/configfiles/fish/
+
+curl -L https://get.oh-my.fish | fish
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+pip install --user powerline-status
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+
+brew install terminal-notifier
+fisher add franciscolourenco/done
+omf install bobthefish
+fisher add brigand/fast-nvm-fish
+fisher add rodrigobdz/fish-apple-touchbar
+
+echo "function fish_greeting; end" > ~/.config/fish/functions/fish_greeting.fish
+
+fish
+set -U fish_user_paths
+
+echo "Don't forget to install the Hack font!"
