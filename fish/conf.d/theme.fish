e487f0a47a72d6e8ad41bda78a8019e681ad6a07
Michael Kohler <me@michaelkohler.info>
2019-04-24 01:35:53 +0200

    fish: split up config files

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -1,10 +1,5 @@
-
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme terminal-dark
 set -g theme_display_hg yes
 set -g theme_display_k8s_context no
 set -g theme_show_exit_status yes
-
-set -g fish_prompt_pwd_dir_length 0
-
-nvm use 12.0.0
