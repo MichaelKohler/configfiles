c80d8575607faaea79257b71943dbc0e033b1fc8
Michael Kohler <me@michaelkohler.info>
2019-11-24 14:02:19 +0100

    Remove s3sync aliases

--- fish/conf.d/zz__aliases.fish
+++ fish/conf.d/zz__aliases.fish
@@ -1,6 +1,3 @@
-alias s3sync="aws s3 sync . s3://mk-cloud-files/ --profile default --exclude '._*' --exclude '*/._*' --exclude '.DS_Store' --exclude '*/.DS_Store'"
-alias s3clean="aws s3 rm s3://mk-cloud-files/ --recursive --exclude '*' --include '._*' --include '*/._*' --include '.DS_Store' --include '*/.DS_Store'"
-
 alias flushnetwork="sudo ifconfig en0 down && sudo route flush && sudo ifconfig en0 up"
 
 alias enc="tar -cvf enc.tar.gz enc && gpg -c enc.tar.gz && rm -rf enc enc.tar.gz"
