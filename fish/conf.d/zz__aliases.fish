85147bb142f2507cf084e96f0303bca5192265ea
Michael Kohler <me@michaelkohler.info>
2019-12-19 23:15:51 +0100

    Make dec/enc work with Sync

--- fish/conf.d/zz__aliases.fish
+++ fish/conf.d/zz__aliases.fish
@@ -1,7 +1,7 @@
 alias flushnetwork="sudo ifconfig en0 down && sudo route flush && sudo ifconfig en0 up"
 
-alias enc="tar -cvf enc.tar.gz enc && gpg -c enc.tar.gz && rm -rf enc enc.tar.gz"
-alias dec="gpg -d enc.tar.gz.gpg > enc.tar.gz && tar -xvf enc.tar.gz && rm -f enc.tar.gz"
+alias enc="pushd ~/Desktop && tar -cvf enc.tar.gz enc && gpg -c enc.tar.gz && rm -rf enc enc.tar.gz && popd && mv -f ~/Desktop/enc.tar.gz.gpg ."
+alias dec="gpg -d enc.tar.gz.gpg > ~/Desktop/enc.tar.gz && tar -xvf ~/Desktop/enc.tar.gz -C ~/Desktop/ && rm -f ~/Desktop/enc.tar.gz"
 
 alias bear="cd 🐻"
 alias k="kubectl"
