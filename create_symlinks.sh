91d16a1f2b8c6219d5e5e8124aa76ba7129a0f87
Michael Kohler <me@michaelkohler.info>
2020-04-16 20:10:15 +0200

    Use .config path for VS Code settings file

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s ~/configfiles/vcode/settings.json "~/Library/Application Support/Code - Insiders/User/settings.json"
+ln -s ~/configfiles/vcode/settings.json "~/.config/Code - Insiders/User/settings.json"
 ln -s ~/configfiles/.ssh_config ~/.ssh/config
 ln -s ~/configfiles/.gitconfig ~/.gitconfig
 ln -s ~/configfiles/.hgrc ~/.hgrc
