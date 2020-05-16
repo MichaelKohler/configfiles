76344de93fd218ba58a4dc64bb588d520a7454d9
Michael Kohler <me@michaelkohler.info>
2020-05-16 14:28:50 +0200

    More cleanup

--- proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-INTERFACE=Wi-Fi
-sudo -v
-sudo networksetup -setsocksfirewallproxy $INTERFACE 127.0.0.1 9050 off
-sudo networksetup -setsocksfirewallproxystate $INTERFACE on
-echo "$(tput setaf 64)" # green
-echo "SOCKS proxy 127.0.0.1:9050 enabled."
--- proxydown.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-INTERFACE=Wi-Fi
-sudo -v
-sudo networksetup -setsocksfirewallproxystate $INTERFACE off
-echo "SOCKS proxy 127.0.0.1:9050 disabled."
